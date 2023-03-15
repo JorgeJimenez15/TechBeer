1e521a9a161750b097c1acfd5832e30edb7955da
JorgeJimenez15 <113641562+JorgeJimenez15@users.noreply.github.com>
2023-03-15 11:05:11 -0600

    Update frontend

--- frontend/TechBeer.js
+++ frontend/TechBeer.js
@@ -7,15 +7,14 @@ Chart.register(CategoryScale)
 Chart.register(LinearScale)
 
 export default class TechBeer {
-    constructor(range) {
+    constructor() {
         this.pin = '0000'
-        this.range = range
         this.temperature = {
             number: 41,
             state: 'cold'
         }
         this.sentence = {
-            index: 0,
+            index: Math.floor(Math.random() * 10),
             hot: [
                 'Watch out, it\'s hot as heck!',
                 'Be careful, it\'s steaming hot!',
@@ -27,18 +26,6 @@ export default class TechBeer {
                 'Just wait a sec, it\'s hot as blazes right now',
                 'Careful, it\'s like a hot sauna in there',
                 'It\'s hot enough to start a fire, but it\'ll cool down in no time'
-            ],
-            warm: [
-                'Hold up, this drink needs a bit of time to cool down',
-                'It\'s a bit warm, give it a minute to cool off for the best taste',
-                'It\'s warm, but don\'t worry, it\'ll be even better once it cools down',
-                'It\'s not quite cold yet, give it some time to chill out before sipping',
-                'This drink is a bit warm, take a break and let it cool off',
-                'It\'s a bit tepid, give it some time to chill before you take a sip',
-                'It\'s not quite cold enough to enjoy, wait for it to cool down a bit more',
-                'It\'s warm, but if you give it some time, it\'ll be a refreshing treat',
-                'Take a breather, this drink is warming up, but it\'ll be worth the wait',
-                'It\'s not quite chilled, give it a few more minutes to cool down before drinking'
             ],
             cold: [
                 'This drink is ice-cold and ready to quench your thirst!',
@@ -53,7 +40,6 @@ export default class TechBeer {
                 'It\'s chilled and ready for you, go ahead and take a swig'
             ]
         }
-
         this.chart = new Chart(document.getElementById('temperature-chart'), {
             type: 'line',
             data: {
@@ -67,9 +53,16 @@ export default class TechBeer {
             }
         })
 
-        this.sentence.index = Math.floor(Math.random() * 10)
-        this.sentenceInterval = setInterval(this.updateSentence.bind(this), 10_000)
+        // Update functions
+        this.update()
         this.updateSentence()
+
+        // Intervals
+        this.updateInterval = setInterval(this.update.bind(this), 5_000)
+        this.sentenceInterval = setInterval(this.updateSentence.bind(this), 10_000)
+
+        // Event listeners
+        document.getElementById('update-pin-code-btn').addEventListener('click', this.updatePinCode.bind(this))
     }
 
     async api(action, data) {
@@ -89,25 +82,25 @@ export default class TechBeer {
         return celsius * 1.8 + 32
     }
 
-    updateTemperature() {
-        this.api("get-current-temperature", 0, this.pin).then((response) => {
+    update() {
+        // Update temperature
+        this.api('get-current-temperature', 0, this.pin).then((response) => {
             this.temperature.number = this.convertToFahrenheit(response)
             this.temperature.state = this.temperature.number > this.range ? 'hot' : this.temperature.number <= this.range ? 'warm' : 'cold'
             document.getElementById('temperature').innerText = this.temperature.state === 'cold' ? `🧊 ${this.temperature.number} °F` : `🔥 ${this.temperature.number} °F`
         })
     }
 
-    updatePinCode(value){
-        this.api("update-pin-code", value).then((response) => {
-            if (response.success) {
-                this.pin = value
-                document.getElementById('pin').innerText = this.pin
-            }
-        })
-    }
     updateSentence() {
         if (this.sentence.index === 10) this.sentence.index = 0
 
         document.getElementById('sentence').innerText = this.sentence[this.temperature.state][this.sentence.index++]
+    }
+
+    async updatePinCode() {
+        const newPin = document.getElementById('newpin').value
+        const response = await this.api('update-pin-code', newPin)
+
+        if (response === 'Success') this.pin = newPin
     }
 }
